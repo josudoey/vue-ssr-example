01335db1f245def61212841f3628d7b7731713eb
Joey Fang <josudoey@gmail.com>
2021-10-13 22:01:00 +0800

    add yup example

--- koa/route/base64.mjs
+++ koa/route/base64.mjs
@@ -1,9 +1,14 @@
+import * as yup from 'yup'
 import createDebug from 'debug'
 const debug = createDebug('app:koa:base64')
 
+const encodeQuerySchema = yup.object().shape({
+  v: yup.string().default('')
+})
 export async function encode (ctx, next) {
   debug('encode', ctx.query.v)
-  const text = ctx.query.v || ''
+  const query = await encodeQuerySchema.validate(ctx.query)
+  const text = query.v
   const result = Buffer.from(text).toString('base64')
   ctx.status = 200
   ctx.body = {
