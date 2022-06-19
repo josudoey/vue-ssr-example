2b4e3a98305603c9c1f3d29f5451f06a5b11506f
Joey Fang <josudoey@gmail.com>
2022-06-19 23:53:36 +0800

    feat: add server package

--- main.mjs
+++ main.mjs
@@ -1,81 +1,8 @@
-import http from 'http'
 import env from './env.cjs'
-import {
-  ExampleVue3, ExampleVue2,
-  createApp, createRouter, createBrowserStatic,
-  createSocketIo, extendKoaIo, getKoaSession
-} from '~example-koa/index.mjs'
-import createExampleVue2SSR from '~example-vue2/create-ssr.mjs'
-import createRxampleVue2Manifest from '~example-vue2/create-manifest.mjs'
-import createExampleVue3SSR from '~example-vue3/create-ssr.mjs'
-import createRxampleVue3Manifest from '~example-vue3/create-manifest.mjs'
-
-const { publicPath, exampleVue2, exampleVue3 } = env
+import { createServer } from '~server'
 
 ;(async function main () {
-  const app = createApp({})
-  const router = createRouter()
-  app.use(router.routes())
-  app.use(createBrowserStatic({
-    browserOutputPath: exampleVue3.browserOutputPath,
-    publicPath
-  }))
-  app.use(createBrowserStatic({
-    browserOutputPath: exampleVue2.browserOutputPath,
-    publicPath
-  }))
-
-  const exampleVue3SSR = await createExampleVue3SSR({ ssrPath: env.exampleVue3.ssrPath })
-  const exampleVue3Manifest = createRxampleVue3Manifest({ manifestPath: env.exampleVue3.manifestPath })
-  ExampleVue3.install(app, {
-    ...exampleVue3SSR,
-    manifest: exampleVue3Manifest
-  })
-
-  const exampleVue2Manifest = createRxampleVue2Manifest({
-    vueSSRClientManifestPath: env.exampleVue2.vueSSRClientManifestPath
-  })
-  const exampleVue2SSR = createExampleVue2SSR({
-    ssrPath: env.exampleVue2.ssrPath
-  })
-  ExampleVue2.install(app, {
-    ...exampleVue2SSR,
-    clientManifest: exampleVue2Manifest
-  })
-  app.use(router.allowedMethods())
-
-  const server = http.createServer(app.callback())
-  server.on('listening', async function () {
-    const address = server.address()
-    const port = address.port
-    console.log(`service listen on ${address.address}:${port}`)
-  })
-  const io = createSocketIo(server)
-
-  extendKoaIo(app.context, io)
-  io.use(async (socket, next) => {
-    const session = await getKoaSession(app, socket.request)
-    const { auth } = session
-    if (!auth) {
-      const err = new Error('unauthorized')
-      next(err)
-      return
-    }
-
-    socket.data.auth = auth
-    next()
-  })
-
-  // https://socket.io/docs/v4/server-instance/#fetchsockets
-  setInterval(function () {
-    io.to('time').emit('now', Date.now())
-  }, 1000)
-
-  io.on('connection', (socket) => {
-    // see https://socket.io/docs/v4/server-socket-instance/#events
-
-    socket.join('time')
-  })
+  const server = await createServer(env)
 
   server.listen(3000)
 })()
---
+++ packages/server/build.mjs
@@ -0,0 +1,23 @@
+import exampleVue2WebpackMain from '~example-vue2-webpack/main.mjs'
+import exampleVue3WebpackMain from '~example-vue3-webpack/main.mjs'
+
+const exampleVue2Build = async ({ publicPath, exampleVue2 }) => {
+  const { browserOutputPath, vueSSRClientManifestPath, ssrOutputPath } = exampleVue2
+  return exampleVue2WebpackMain({
+    publicPath, browserOutputPath, vueSSRClientManifestPath, ssrOutputPath
+  })
+}
+
+const exampleVue3Build = async ({ publicPath, exampleVue3 }) => {
+  const { browserOutputPath, manifestPath, ssrOutputPath } = exampleVue3
+  return exampleVue3WebpackMain({
+    publicPath, browserOutputPath, manifestPath, ssrOutputPath
+  })
+}
+
+export default function (env) {
+  return Promise.all([
+    exampleVue2Build(env),
+    exampleVue3Build(env)
+  ])
+}
