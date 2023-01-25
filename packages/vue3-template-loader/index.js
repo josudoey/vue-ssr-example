6b602c90a74de8c548e1128b50d2153c0f151d7e
Joey Fang <josudoey@gmail.com>
2023-01-25 20:18:38 +0800

    refactor: remove querystring use for vue3-template-loader

--- packages/vue3-template-loader/index.js
+++ packages/vue3-template-loader/index.js
@@ -1,15 +1,14 @@
 const TemplateLoader = require('vue-loader/dist/templateLoader.js')
-const qs = require('querystring')
 const uniqueId = require('lodash/uniqueId')
 module.exports.default = function (source) {
   const loaderContext = this
-  const query = qs.parse(loaderContext.resourceQuery.slice(1))
-  if (!query.id) {
+  const urlSearchParams = new URLSearchParams(loaderContext.resourceQuery.slice(1))
+  if (!urlSearchParams.get('id')) {
     // handle issue
     // - [@vue/compiler-sfc] compileTemplate now requires the `id` option.`.
     // - https://github.com/vuejs/core/blob/13fc8ff9dd7007e4ef7d2f0b6e5c973013ccc8b8/packages/compiler-sfc/src/compileTemplate.ts#L189-L212
-    query.id = uniqueId('v')
-    loaderContext.resourceQuery = `?${qs.stringify(query)}`
+    urlSearchParams.set('id', uniqueId('v'))
+    loaderContext.resourceQuery = `?${urlSearchParams.toString()}`
   }
   return TemplateLoader.default.call(this, source)
 }
